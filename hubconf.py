11de9810e0d40768dbe2bfaeea913f7ec8cb1a2d
cs18b015iittp <79402448+cs18b015iittp@users.noreply.github.com>
2022-10-19 10:02:30 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -13,6 +13,7 @@ def load_data():
         root="data",
         train=False,
         download=False,
+     
         transform=ToTensor(),
     )
     
@@ -22,6 +23,7 @@ class ModifiedDataset(Dataset):
   def __init__(self,given_dataset,shrink_percent=10):
     self.given_dataset = given_dataset
     self.shrink_percent = shrink_percent
+    
     
   def __len__(self):
     return len(self.given_dataset)
