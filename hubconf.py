21c842d7526859c9ebd6e6d428802a1f92d8edc0
cs18b015iittp <79402448+cs18b015iittp@users.noreply.github.com>
2022-10-19 09:35:49 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,1 +1,19 @@
+def load_data():
 
+ 
+    training_data = datasets.FashionMNIST(
+        root="data",
+        train=True,
+        download=False,
+        transform=ToTensor(),
+    )
+
+   
+    test_data = datasets.FashionMNIST(
+        root="data",
+        train=False,
+        download=False,
+        transform=ToTensor(),
+    )
+    
+    return training_data, test_data
