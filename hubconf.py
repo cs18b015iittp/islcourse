03ae4f75c79984b3d8c8675a3e04a1c2a46e663e
cs18b015iittp <79402448+cs18b015iittp@users.noreply.github.com>
2022-10-19 09:47:41 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -17,3 +17,14 @@ def load_data():
     )
     
     return training_data, test_data
+   
+class ModifiedDataset(Dataset):
+  def __init__(self,given_dataset,shrink_percent=10):
+    self.given_dataset = given_dataset
+    self.shrink_percent = shrink_percent
+    
+  def __len__(self):
+    return len(self.given_dataset)
+
+  def __getitem__(self,idx):
+    img, lab = self.given_dataset[idx]
